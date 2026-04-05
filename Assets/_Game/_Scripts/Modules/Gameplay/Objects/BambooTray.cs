6bb82fd47836847ce38d1574779b1d0733c82228
Dang Quoc Hieu <dqh081004@gmail.com>
2026-04-06 00:00:35 +0700

    add herb

--- Assets/_Game/_Scripts/Modules/Gameplay/Objects/BambooTray.cs
+++ Assets/_Game/_Scripts/Modules/Gameplay/Objects/BambooTray.cs
@@ -14,23 +14,55 @@ public class BambooTray : GrabbableObject
     public Transform sauceAnchor;
 
     [Title("Tofu")]
+    public Transform tofuAnchorParent;
     public List<AnchorPoint> tofuAnchorPoints = new();
 
     [Title("Rice Noodle")]
+    public Transform riceNoodleAnchorParent;
     public List<AnchorPoint> riceNoodleAnchorPoints = new();
 
     [Title("Cucumber")]
+    public Transform cucumberAnchorParent;
     public List<AnchorPoint> cucumberAnchorPoints = new();
+
+    [Title("Herb")]
+    public Transform herbAnchor;
+    public List<AnchorPoint> herbAnchorPoints = new();
 
     protected override void Awake()
     {
         base.Awake();
+        InitializeAnchorPoints();
         anchorPointsDic.Add(IngredientType.Tofu, tofuAnchorPoints);
         anchorPointsDic.Add(IngredientType.RiceNoodle, riceNoodleAnchorPoints);
         anchorPointsDic.Add(IngredientType.Cucumber, cucumberAnchorPoints);
+        anchorPointsDic.Add(IngredientType.Herb, herbAnchorPoints);
+
     }
 
-
+    private void InitializeAnchorPoints()
+    {
+        for (int i = 0; i < tofuAnchorParent.childCount; i++)
+        {
+            var anchorPoint = new AnchorPoint() { anchor = tofuAnchorParent.GetChild(i), isEmpty = true };
+            tofuAnchorPoints.Add(anchorPoint);
+        }
+        for (int i = 0; i < riceNoodleAnchorParent.childCount; i++)
+        {
+            var anchorPoint = new AnchorPoint() { anchor = riceNoodleAnchorParent.GetChild(i), isEmpty = true };
+            riceNoodleAnchorPoints.Add(anchorPoint);
+        }
+        for (int i = 0; i < cucumberAnchorParent.childCount; i++)
+        {
+            var anchorPoint = new AnchorPoint() { anchor = cucumberAnchorParent.GetChild(i), isEmpty = true };
+            cucumberAnchorPoints.Add(anchorPoint);
+        }
+        for (int i = 0; i < herbAnchor.childCount; i++)
+        {
+            var anchorPoint = new AnchorPoint() { anchor = herbAnchor.GetChild(i), isEmpty = true };
+            herbAnchorPoints.Add(anchorPoint);
+        }
+    }
     public AnchorPoint GetAnchorPoint(IngredientType ingredientType)
     {
         var anchorPoint = anchorPointsDic[ingredientType];
